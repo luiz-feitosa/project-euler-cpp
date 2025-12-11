db473202bcc96168babf94b200cb65ed1bbc4789
luiz-feitosa <feitosa@protonmail.com>
2025-12-11 08:45:54 +0100

    refactor(problem-1): enhance readability with TARGET constant

--- p0001.cpp
+++ p0001.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 int main() {
-  int sum = 0;
-  for (int i = 0; i < 1000; ++i) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      sum += i;
+    const int TARGET = 1000;
+    int sum = 0;
+    for (int i = 0; i < TARGET; ++i) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
     }
-  }
-  std::cout << sum << std::endl;
-  return 0;
+    std::cout << sum << std::endl;
+    return 0;
 }
