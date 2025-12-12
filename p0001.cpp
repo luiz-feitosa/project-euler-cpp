5201df8f4b387578de4da9637404fda3f62f6cf7
luiz-feitosa <feitosa@protonmail.com>
2025-12-12 16:56:11 +0100

    refactor(problem-1): enhance readability with blank lines

--- p0001.cpp
+++ p0001.cpp
@@ -3,11 +3,13 @@
 int main() {
     const int TARGET = 1000;
     int sum = 0;
+
     for (int i = 0; i < TARGET; ++i) {
         if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
     }
+
     std::cout << sum << std::endl;
     return 0;
 }
