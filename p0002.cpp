7fd8e54a282ce040a9a5e0c5c86f1dc044a8fbac
luiz-feitosa <feitosa@protonmail.com>
2025-12-12 17:22:54 +0100

    feat(problem): add solution for problem 2

---
+++ p0002.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    const int TARGET = 4'000'000;
+    int a = 1;
+    int b = 2;
+    int c = 0;
+    int sum = 0;
+
+    while (b <= TARGET) {
+        if (b % 2 == 0) {
+            sum += b;
+        }
+    c = a + b;
+    a = b;
+    b = c;
+    }
+
+    std::cout<<sum<<std::endl;
+    return 0;
+}
